885ef728e0089c770745a84ef51a168b8730df98
VonTum <lennart.vanhirtum@gmail.com>
2020-04-05 19:26:26 +0200

    Altered restraining bolt

--- src/AdvancedRestrainingBolt.java
+++ src/AdvancedRestrainingBolt.java
@@ -4,10 +4,13 @@ public class AdvancedRestrainingBolt implements RestrainingBolt{
 	private Environment env;
 	private Agent agent;
 	
+	//1 if all our plants have ever been watered, at any time, 0 otherwise
+	public boolean isAllowedToWaterNeighbor;
+	
 	public AdvancedRestrainingBolt(Environment env, Agent agent) {
 		this.env = env;
 		this.agent = agent;
-		this.setState(0);
+		this.isAllowedToWaterNeighbor = false;
 	}	
 	
 	@Override
@@ -15,51 +18,38 @@ public class AdvancedRestrainingBolt implements RestrainingBolt{
 		return 2;
 	}
 	
-	//1 if all our plants are watered, 0 otherwise
-	public int state;
-	
-	public void setState(int i){
-		this.state = i;
-	}
-	
 	@Override
 	public int getCurrentState() {
-		return this.state;
+		return this.isAllowedToWaterNeighbor ? 1 : 0;
 	}
-
+	
 	//return -10 if watering neighbour plants when our plants are not all watered
 	@Override
 	public double applyAction(int action) {
-		if (checkState()){
-			return -10;
+		if(areAllOurTomatoesWatered()) {
+			this.isAllowedToWaterNeighbor = true;
 		}
-		else return 0;
+		if (!this.isAllowedToWaterNeighbor){
+			Position nextPos = env.getPositionWhenMoving(agent.x, agent.y, Move.values()[action]);
+			if (this.agent.x > env.ourTomatoesWidth+1 && this.env.getTile(nextPos.x, nextPos.y) == Tile.UNWATERED_TOMATO){
+				return -20;
+			}
+		}
+		return 0;
 	}
 	
-	//true if all our tommatoes are watered
-	private boolean checkOurTomatoesWatered(){
-		boolean b = true;
-		for (int i = 0; i <= 3; i++){
-			for (int j = 0; j <= this.env.getHeight(); j++){
-				if (this.env.getTile(i, j) == Tile.UNWATERED_TOMATO) b = false;
+	//true if all our tomatoes are watered
+	private boolean areAllOurTomatoesWatered(){
+		for (int x = 1; x <= env.ourTomatoesWidth+1; x++){
+			for (int y = 1; y <= this.env.getHeight() - 1; y++){
+				if (this.env.getTile(x, y) == Tile.UNWATERED_TOMATO) return false;
 			}		
 		}
-		this.setState(b ? 1 : 0);
-		return b;
+		return true;
 	}
 	
-	//true if we try to water a neighbours tomato when not all our tomatoes are watered 
-	private boolean checkState() {
-		boolean b = false;
-		//assumes our tomatoes are on x <= 3
-		if (this.agent.x > 3 && this.env.getTile(this.agent.x, this.agent.y) == Tile.UNWATERED_TOMATO){
-			if (checkOurTomatoesWatered()) b = true;					
-		}
-		return b;
-	}
-
 	@Override
 	public void reset() {
-		this.setState(0);
+		this.isAllowedToWaterNeighbor = false;
 	}
 }
